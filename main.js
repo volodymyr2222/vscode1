6a856df145b71cab3390aaf014acb395ebf7cf36
volodymyr2222 <volody9089@gmail.com>
2021-03-07 18:22:18 +0200

    task 42

--- main.js
+++ main.js
@@ -39,3 +39,69 @@ console.log(srt);
 srt = Number(srt);
 console.log(typeof srt);
 
+
+
+let nam = 12;
+console.log(nam);
+
+let nam = String(nam);
+console.log(typeof nam);
+
+
+// taska 3
+
+let dataTypes = true;
+console.log(typeof dataTypes);
+
+dataTypes = String(dataTypes);
+console.log(typeof dataTypes);
+
+
+
+
+
+
+let dataTypes = 22;
+console.log(dataTypes);
+
+let dataTypes = String(dataTypes);
+console.log(typeof dataTypes);
+
+
+
+
+
+
+let dataTypes = Symbol("ad");
+console.log(typeof dataTypes);
+
+let dataTypes = String(dataTypes);
+console.log(typeof dataTypes);
+
+
+
+
+
+let dataTypes = null;
+console.log(typeof dataTypes);
+
+let dataTypes = String(dataTypes);
+console.log(typeof dataTypes);
+
+
+
+
+
+
+let dataTypes = 22n;
+console.log(typeof dataTypes);
+
+let dataTypes = String(dataTypes);
+console.log(typeof dataTypes);
+
+
+
+
+
+
+
