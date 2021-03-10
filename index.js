b81f0d622a35f73de84a1df729bc441617f3159c
volodymyr2222 <volody9089@gmail.com>
2021-03-10 16:29:41 +0200

    taska 10

---
+++ index.js
@@ -0,0 +1,14 @@
+//taska 1 (функція)
+function factorial(n) {
+    return (n != 1) ? n * factorial(n - 1) : 1;
+  }
+  
+  console.log( factorial(2) ); 
+
+  //taska 2 
+  let condition = (year == 2015); // преобразуется к true или false
+
+  if (condition) {
+    console.log(condition)
+  }
+  
